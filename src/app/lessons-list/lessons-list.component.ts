591ff4a952ae296a71653de88fae84dd7ef2ae03
Alex Colombo <uricaine@hotmail.com>
2019-10-29 15:27:17 +0100

    creating lesson list component

--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,3 +1,4 @@
+import { Lesson } from './../shared/model/lesson';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lessons-list.component.css']
 })
 export class LessonsListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  lessons: Lesson[] = [];
+  constructor() {
+    this.lessons = [
+      {
+        id: 1,
+        description: 'lesson one',
+        duration: '3 minutes'
+      },
+      {
+        id: 2,
+        description: 'lesson two',
+        duration: '2 minutes'
+      },
+      {
+        id: 3,
+        description: 'lesson three',
+        duration: '1 minutes'
+      }
+    ];
   }
 
+  ngOnInit() {}
 }
