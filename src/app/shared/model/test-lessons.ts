7fd48fb7eb7227547e4bc754b3331d80f20fd35c
Alex Colombo <uricaine@hotmail.com>
2019-10-29 15:27:39 +0100

    creating lesson data

---
+++ src/app/shared/model/test-lessons.ts
@@ -0,0 +1,18 @@
+import { Lesson } from './lesson';
+export const lessons: Lesson[] = [
+  {
+    id: 1,
+    description: 'lesson one',
+    duration: '3 minutes'
+  },
+  {
+    id: 2,
+    description: 'lesson two',
+    duration: '2 minutes'
+  },
+  {
+    id: 3,
+    description: 'lesson three',
+    duration: '1 minutes'
+  }
+];
