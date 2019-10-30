3fed34cb8320e9d7f35cf09bc8dcafac73d4e588
Alex Colombo <uricaine@hotmail.com>
2019-10-30 20:28:37 +0100

    refactor component

--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,3 +1,4 @@
+import { globalEventBus, Observer } from './../event-bus-experiments/event-bus';
 import { Lesson } from './../shared/model/lesson';
 import { Component, OnInit } from '@angular/core';
 
@@ -6,27 +7,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './lessons-list.component.html',
   styleUrls: ['./lessons-list.component.css']
 })
-export class LessonsListComponent implements OnInit {
+export class LessonsListComponent implements Observer {
   lessons: Lesson[] = [];
   constructor() {
-    this.lessons = [
-      {
-        id: 1,
-        description: 'lesson one',
-        duration: '3 minutes'
-      },
-      {
-        id: 2,
-        description: 'lesson two',
-        duration: '2 minutes'
-      },
-      {
-        id: 3,
-        description: 'lesson three',
-        duration: '1 minutes'
-      }
-    ];
+    console.log('lesson list component is registeres as observer ...');
+    globalEventBus.registerObserver(this);
   }
 
-  ngOnInit() {}
+  notify(data: Lesson[]) {
+    console.log('Lesson list component received data ....');
+    this.lessons = data;
+  }
 }
